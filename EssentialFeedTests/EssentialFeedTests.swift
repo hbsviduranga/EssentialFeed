55d5e8d47d8fcb97b40ffcd4dfe83e636a33b280
Sumeda Viduranga <hbsumeda@gmail.com>
2024-11-24 18:14:43 +0530

    Initial Commit

---
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedTests.swift
+//  EssentialFeedTests
+//
+//  Created by Sumeda Viduranga on 2024-11-24.
+//
+
+import Testing
+@testable import EssentialFeed
+
+struct EssentialFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
